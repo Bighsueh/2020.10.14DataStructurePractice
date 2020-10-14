debbfa24b4817757f01d295c7cc376a0016441ce
40841121 <40841121@gm.nfu.edu.tw>
2020-10-14 10:29:20 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/a20201014datastructurepractice/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.a20201014datastructurepractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+TextView textView;
+Button button;
+EditText editText;
+int num;
+Random ran=new Random();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+        button=findViewById(R.id.button);
+        button.findViewById(R.id.button);
+        button.setOnClickListener((v){
+            textView.setText("");
+        num = Integer.parseInt(editText.getText().toString());
+        int data[] = new int[num];
+        for (int i = 0; i <= data.length - 1; i++) {
+            data[i]=ran.nextInt(49);
+            if(i!=0){textView.setText(textView.getText().toString()+",");
+                textView.setText(textView.getText().toString() + Integer.toString(data[i]));
+        }
+        });
+
+    }
+}
